24efbf4deeb19fbbf4c6b557966d0f6b0d835b92
Felipe Godoy <felipe.godoy@ceinf.cl>
2025-05-12 23:42:44 -0400

    test for config class

--- __main__.py
+++ __main__.py
@@ -3,16 +3,16 @@ from src.config import Config
 from src.hikvision_event_processor import HikvisionEventProcessor
 from src.utils import config_logger
 
-# Agregamos mensajes de depuración para verificar la inicialización de Config
-try:
-    config = Config()
-    print("Configuración inicializada correctamente.")
-except Exception as e:
-    print(f"Error al inicializar la configuración: {str(e)}")
-
 # Instanciacion de clases necesarias
 hikvsionEventProcessor = HikvisionEventProcessor()
 logger = config_logger()
+
+# Usamos el logger para registrar mensajes de depuración
+try:
+    config = Config()
+    logger.info("Configuración inicializada correctamente.")
+except Exception as e:
+    logger.error(f"Error al inicializar la configuración: {str(e)}")
 
 if __name__ == "__main__":
     try:
