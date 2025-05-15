7789ba80b28ace315b749c221eae50eeef60e8b2
Felipe Godoy <felipe.godoy@ceinf.cl>
2025-05-14 21:13:33 -0400

    clean innecesary code

--- __main__.py
+++ __main__.py
@@ -6,13 +6,6 @@ from src.logger import logger
 # Instanciacion de clases necesarias
 hikvsionEventProcessor = HikvisionEventProcessor()
 
-# Usamos el logger para registrar mensajes de depuración
-try:
-    config.print_vars()
-    logger.info("Configuración inicializada correctamente.")
-except Exception as e:
-    logger.error(f"Error al inicializar la configuración: {str(e)}")
-
 if __name__ == "__main__":
     try:
         hikvsionEventProcessor.listen()
